4327a6f4d1e14158f162c8c135e48c8904dc7d25
alanmacdoug <82167852+alanmacdoug@users.noreply.github.com>
2026-09-12 08:50:05 +0100

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -5,14 +5,14 @@ export const headerData = {
     { text: 'Services', href: getPermalink('/services') },
     { text: 'About', href: getPermalink('/about') },
     { text: 'Contact', href: getPermalink('/contact') },
-    {
-      text: 'Tools',
-      items: [
-        { text: 'Authentication checker', href: getPermalink('/tools') },
-        { text: 'Cost calculator', href: getPermalink('/roi-calculator') },
-        { text: 'Mautic vs ESP comparison', href: getPermalink('/mautic-cost-comparison') },
-      ],
-    },
+   {
+  text: 'Tools',
+  links: [
+    { text: 'Authentication checker', href: getPermalink('/tools') },
+    { text: 'Cost calculator', href: getPermalink('/roi-calculator') },
+    { text: 'Mautic vs ESP comparison', href: getPermalink('/mautic-cost-comparison') },
+  ],
+},
     { text: 'Blog', href: getBlogPermalink() },
   ],
   actions: [
